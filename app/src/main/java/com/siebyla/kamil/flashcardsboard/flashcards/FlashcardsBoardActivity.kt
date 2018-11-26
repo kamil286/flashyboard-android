90532df78f77c017ab5ec5a4d13603f062641e54
kamil286 <siebylakamil@gmail>
2018-11-26 20:35:01 +0100

    Remove unnecessary imports.

--- app/src/main/java/com/siebyla/kamil/flashcardsboard/flashcards/FlashcardsBoardActivity.kt
+++ app/src/main/java/com/siebyla/kamil/flashcardsboard/flashcards/FlashcardsBoardActivity.kt
@@ -4,12 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ListView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
